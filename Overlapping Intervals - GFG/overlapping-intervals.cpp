41dfdf0881ad955cd24b2a3b1ae149b42960713e
fullmetal-heart <59385811+fullmetal-heart@users.noreply.github.com>
2022-10-13 01:31:21 +0530

    Added solution - LeetHub

---
+++ Overlapping Intervals - GFG/overlapping-intervals.cpp
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+public:
+    vector<vector<int>> overlappedInterval(vector<vector<int>>& intervals) {
+         // Code here
+    sort(intervals.begin(),intervals.end());
+    vector<vector<int>> ans;
+    stack<vector<int>> stk;
+    vector<int> temp=intervals[0];
+    
+    stk.push(temp);
+    
+    for(int i=1;i<intervals.size();i++)
+    {
+        
+        if(intervals[i][0]<=stk.top()[1])
+        {
+            stk.top()[1]=max(intervals[i][1],stk.top()[1]);
+        }
+        else
+        {
+            vector<int> temp= intervals[i];
+            stk.push(temp);
+        }
+    }
+    
+    while(!stk.empty())
+    {
+        auto temp=stk.top();
+        ans.push_back(temp);
+        stk.pop();
+    }
+    sort(ans.begin(),ans.end());
+    
+    return ans;
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n;
+		cin >> n;
+		vector<vector<int>>Intervals(n);
+		for(int i = 0; i < n; i++){
+			int x, y;
+			cin >> x >> y;
+			Intervals[i].push_back(x);
+			Intervals[i].push_back(y);
+		}
+		Solution obj;
+		vector<vector<int>> ans = obj.overlappedInterval(Intervals);
+		for(auto i: ans){
+			for(auto j: i){
+				cout << j << " ";
+			}
+		}
+		cout << "\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
